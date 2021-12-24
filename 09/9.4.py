9beea65c28926a299a83c1ab489d72bea729c08c
abe-101 <abe@habet.dev>
2021-12-24 10:32:29 -0500

    9.4.py

---
+++ 09/9.4.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""
+Exercise 9.4. Write a function named uses_only that takes a word and a string of letters, and
+that returns True if the word contains only letters in the list. Can you make a sentence using only
+the letters acefhlo? Other than “Hoe alfalfa”?
+"""
+def uses_only(word, available):
+    for letter in word:
+        if letter not in available:
+            return False
+    return True
